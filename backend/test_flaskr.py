715a1a8753d6ea16fe16b6d240a50e8863e47bfe
Tim Evans <evans.tim@gmail.com>
2022-09-09 09:48:48 +0100

    "dialect" warning solved for unit tests. Added test for high Qs pagination.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name) # "sql" here after "postgres" stops the "dialect name" warning
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -34,6 +34,12 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_too_high_quest_pagination(self):
+        res = self.client().get("")
+        data = json.loads(res.data)
+        
+        self.assertEqual(data["message","Not found"])
+        self.assertEqual(res.status_code,404)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":

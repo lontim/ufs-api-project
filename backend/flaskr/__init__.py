079527d88a8670c2fd4419281461ccec6380f4c8
Tim Evans <evans.tim@gmail.com>
2022-09-09 13:02:50 +0100

    question deletion handler - check if question exists

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,3 +1,4 @@
+from http.client import OK
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -99,12 +100,24 @@ def create_app(test_config=None):
         )
 
     """
-    @TODO:
-    Create an endpoint to DELETE question using a question ID.
+    An endpoint to DELETE question using a question ID.
 
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+
+    @app.route("/questions/<int:id>", methods=['DELETE'])
+    def delete_question(id):
+        question = Question.query.get(id)
+        if not question:
+            abort(404)
+
+        return jsonify(
+            {
+                "success": True,
+                "deleted": id,
+            }
+        )
 
     """
     @TODO:
